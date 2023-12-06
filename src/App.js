e4ae08df9865d3fdc816a24cdeb24e61bd2f2cba
OwenSyKim <owen.sykim@gmail.com>
2023-12-05 16:38:00 -0800

    Add login prompt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import socket from "./server";
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    askUserName();
+  }, []);
+  const askUserName = () => {
+    const userName = prompt("Please Text your Name");
+    console.log("uuu", userName);
+
+    socket.emit("login", userName, (res) => {
+      if (res?.ok) {
+        setUser(res.data);
+      }
+    });
+  };
+
   return (
     <div>
       <div className="App"></div>
